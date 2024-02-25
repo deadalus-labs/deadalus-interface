600bc574492582d8bc5ba06caf16fc893b8ad083
supreme2580 <victoromorogbe69@gmail.com>
2024-02-25 11:04:30 +0100

    counter deposit

--- src/components/counter-detail.tsx
+++ src/components/counter-detail.tsx
@@ -56,7 +56,7 @@ const CounterDetail = () => {
 				</div>
 				<div className='flex-1 h-fit border-2 border-slate-800 rounded-lg'>
 					<Tabs defaultValue="history">
-						<TabsList className='bg-transparent border-b-2 border-slate-800 w-full flex justify-start'>
+						<TabsList className='bg-transparent border-b-2 border-slate-800 w-full flex justify-start rounded-b-none'>
 							<TabsTrigger value="history" className='data-[state=active]:bg-transparent data-[state=active]:text-slate-400 
 							data-[state=active]:underline data-[state=active]:underline-offset-[13.5px] data-[state=active]:decoration-[#16A24A] 
 							font-medium text-base'>Access History</TabsTrigger>
@@ -73,7 +73,8 @@ const CounterDetail = () => {
 									</TableRow>
 								</TableHeader>
 								<TableBody>
-									{counters.map((counter, i) => <TableRow key={i} className='hover:bg-transparent border-slate-800 border-2 flex justify-between items-center'>
+									{counters.map((counter, i) => <TableRow key={i} className='hover:bg-transparent border-slate-800 border-t-2
+									flex justify-between items-center'>
 										<TableCell className='text-base font-normal text-slate-50'>{counter}</TableCell>
 										<TableCell className='text-base font-normal text-slate-50'>{counter}</TableCell>
 									</TableRow>
