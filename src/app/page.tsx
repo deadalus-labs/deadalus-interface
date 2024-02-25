65e7bca80c222495453e8403dde6aaec03a3ef41
supreme2580 <victoromorogbe69@gmail.com>
2024-02-25 10:51:34 +0100

    counter deposit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import CounterDeposit from "@/components/counter-deposit";
+import CounterDetail from "@/components/counter-detail";
 import CounterFractionalizer from "@/components/counter-fractionalize";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen">
-      <CounterDeposit />
+      <CounterDetail />
     </main>
   );
 }
--- src/components/counter-detail.tsx
+++ src/components/counter-detail.tsx
@@ -1,39 +1,106 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
 import {
 	Card,
 	CardContent,
-	CardDescription,
-	CardFooter,
-	CardHeader,
-	CardTitle,
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
+import CandleStickIcon from './icons/candle-stick-icon';
+import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell } from './ui/table';
 
 const CounterDetail = () => {
-  return (
-		<Card className="flex flex-col lg:w-1/2 mx-auto bg-[#111827] p-5 gap-y-4 text-white">
-			<div className="grid grid-cols-2 gap-4">
-				<div>
-					<CardHeader className="flex justify-center border border-slate-300 rounded-lg">
-						<div className="flex justify-center items-center w-full gap-x-2">
-							<Button className="bg-green-500">Increment</Button>
-							<Button className="bg-green-500">Decrement</Button>
-						</div>
-					</CardHeader>
-					<CardContent className="border rounded-lg mt-4">
-						<div className="flex mt-2">
-							<h2>Error Message</h2>
-						</div>
-					</CardContent>
-				</div>
-				<div>
-					<CardFooter className="flex justify-between border border-slate-300 rounded-lg text-white">
-						<div className="flex mt-2">
-							<h2>Your Counters</h2>
-						</div>
-					</CardFooter>
-				</div>
+	const [count, setCount] = useState(0);
+
+	const incrementFunction = () => {
+		setCount(count + 1);
+	}
+
+	const decrementFunction = () => {
+		if (count > 0) {
+			setCount(count - 1);
+		}
+	}
+
+	const counters = [
+		"Counter 1",
+		"Counter 2",
+		"Counter 3",
+		"Counter 4",
+	]
+	return (
+		<Card className="flex flex-col w-full mx-auto text-white bg-[#111827A6]/65 p-5 space-y-6 border-0">
+			<div className='w-full flex items-center justify-between'>
+				<h1 className="text-2xl font-semibold">Fractionalize Counter</h1>
+				<Button className='bg-[#16A24A] focus:bg-[#16A24A] hover:bg-[#16A24A]'>
+					<CandleStickIcon />
+				</Button>
 			</div>
+			<CardContent className='flex w-full p-0 md:space-x-6 flex-col space-y-6 md:flex-row md:space-y-0'>
+				<div className='flex-1 space-y-6 h-full'>
+					<div className='h-64 border-2 border-slate-800 rounded-lg'>
+						<div className='w-full h-full flex flex-col items-center justify-center space-y-6'>
+							<div className='border-2 border-slate-800 rounded-lg w-72 p-3.5 text-center'>
+								<p>{count}</p>
+							</div>
+							<div className='flex space-x-5 items-center w-72 justify-between'>
+								<Button className='bg-transparent hover:bg-[#16A24A] focus:bg-[#16A24A] flex-1 border-2 border-[#16A24A] text-base' onClick={incrementFunction}>Increment</Button>
+								<Button className='bg-transparent hover:bg-[#16A24A] focus:bg-[#16A24A] flex-1 border-2 border-[#16A24A] text-base' onClick={decrementFunction}>Decrement</Button>
+							</div>
+						</div>
+					</div>
+					<div className='h-fit border-2 border-slate-800 rounded-lg text-center p-5'>
+						<p className='text-slate-300 text-lg'>You currently don{"’"}t have permission to call Counter 1 contract.</p>
+					</div>
+				</div>
+				<div className='flex-1 h-fit border-2 border-slate-800 rounded-lg'>
+					<Tabs defaultValue="history">
+						<TabsList className='bg-transparent border-b-2 border-slate-800 w-full flex justify-start'>
+							<TabsTrigger value="history" className='data-[state=active]:bg-transparent data-[state=active]:text-slate-400 
+							data-[state=active]:underline data-[state=active]:underline-offset-[13.5px] data-[state=active]:decoration-[#16A24A] 
+							font-medium text-base'>Access History</TabsTrigger>
+							<TabsTrigger value="transaction" className='data-[state=active]:bg-transparent data-[state=active]:text-slate-400 
+							data-[state=active]:underline data-[state=active]:underline-offset-[13.5px] data-[state=active]:decoration-[#16A24A] 
+							font-medium text-base'>Counter Transaction</TabsTrigger>
+						</TabsList>
+						<TabsContent value="history">
+							<Table className='w-full'>
+								<TableHeader>
+									<TableRow className='hover:bg-transparent flex border-transparent justify-between items-center'>
+										<TableHead className='text-slate-400 text-lg flex items-center font-normal'>Timestamp</TableHead>
+										<TableHead className='text-slate-400 text-lg flex items-center font-normal'>Action</TableHead>
+									</TableRow>
+								</TableHeader>
+								<TableBody>
+									{counters.map((counter, i) => <TableRow key={i} className='hover:bg-transparent border-slate-800 border-2 flex justify-between items-center'>
+										<TableCell className='text-base font-normal text-slate-50'>{counter}</TableCell>
+										<TableCell className='text-base font-normal text-slate-50'>{counter}</TableCell>
+									</TableRow>
+									)}
+								</TableBody>
+							</Table>
+						</TabsContent>
+						<TabsContent value="transaction">
+							<Table className='w-full'>
+								<TableHeader>
+									<TableRow className='hover:bg-transparent flex border-transparent justify-between items-center'>
+										<TableHead className='text-slate-400 text-lg flex items-center font-normal'>Timestamp</TableHead>
+										<TableHead className='text-slate-400 text-lg flex items-center font-normal'>Action</TableHead>
+									</TableRow>
+								</TableHeader>
+								<TableBody>
+									{counters.map((counter, i) => <TableRow key={i} className='hover:bg-transparent border-slate-800 border-2 flex justify-between items-center'>
+										<TableCell className='text-base font-normal text-slate-50'>{counter}</TableCell>
+										<TableCell className='text-base font-normal text-slate-50'>{counter}</TableCell>
+									</TableRow>
+									)}
+								</TableBody>
+							</Table>
+						</TabsContent>
+					</Tabs>
+				</div>
+			</CardContent>
 		</Card>
 	);
 }
