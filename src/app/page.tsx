14ccbfd288be1d55090308f936b9da5d487e0a09
supreme2580 <victoromorogbe69@gmail.com>
2024-02-25 03:46:00 +0100

    fractionalize-counter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 "use client";
 
+import CounterFractionalizer from "@/components/counter-fractionalize";
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen">
-      <p className="text-lg">
-        Get started by editing&nbsp;
-        <code className="p-1 bg-gray-200 rounded">app/page.tsx</code>
-      </p>
+      <CounterFractionalizer />
     </main>
   );
 }
---
+++ src/components/icons/candle-stick.tsx
@@ -0,0 +1,20 @@
+export default function CandleStick() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+        >
+            <path
+                stroke="#fff"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeMiterlimit="10"
+                strokeWidth="1.5"
+                d="M22 17.5h-7M5 17.5H2M22 6.5h-3M9 6.5H2M7 14.5h6c1.1 0 2 .5 2 2v2c0 1.5-.9 2-2 2H7c-1.1 0-2-.5-2-2v-2c0-1.5.9-2 2-2zM11 3.5h6c1.1 0 2 .5 2 2v2c0 1.5-.9 2-2 2h-6c-1.1 0-2-.5-2-2v-2c0-1.5.9-2 2-2z"
+            ></path>
+        </svg>
+    );
+}
