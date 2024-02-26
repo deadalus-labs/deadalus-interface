4059e63243758d56318842fa7ca00e464c720ff0
Tony Stark <oktonystark8@gmail.com>
2024-02-26 11:10:51 -0700

    fixed input field

--- src/components/counter-deposit.tsx
+++ src/components/counter-deposit.tsx
@@ -33,6 +33,12 @@ import { VAULT_ADDRESS } from "@/lib/constants/contract_address";
 
 
 const CounterDeposit = () => {
+	const [depositAddress, setDepositAddress] = useState<string>("")
+
+	const handleInputChange = (event: any) => {
+		setDepositAddress(event.target.value);
+	};
+
 	const { address } = useAccount();
 	const { contract } = useContract({
 		abi: fractionVaultABI,
@@ -40,12 +46,12 @@ const CounterDeposit = () => {
 	});
 
 	const calls = useMemo(() => {
-		if (!address || !contract) return [];
-		return contract.populateTransaction["deposit_contract"]!(address, {
+		if (!depositAddress || !contract) return [];
+		return contract.populateTransaction["deposit_contract"]!(depositAddress, {
 			low: 1,
 			high: 0,
 		});
-	}, [contract, address])
+	}, [contract, depositAddress]);
 
 	const { writeAsync, data, isPending } = useContractWrite({
 		calls,
@@ -59,6 +65,8 @@ const CounterDeposit = () => {
 					<div className="flex flex-col items-center justify-center space-y-1 w-2/3">
 						<Input
 							type="text"
+							value={depositAddress}
+							onChange={handleInputChange}
 							placeholder="Deposit Cairo Contract"
 							className="w-full bg-transparent px-5 py-7 border-slate-800 focus-visible:ring-offset-0 focus-visible:ring-0"
 						/>
