93151117c47667fd4c4a7a131f10e0887459450f
phipsae <philip.krause88@yahoo.de>
2024-02-26 21:08:52 -0700

    added disable button when tx is not confirmed

--- src/components/flat-deposit.tsx
+++ src/components/flat-deposit.tsx
@@ -21,7 +21,7 @@ import {
 } from "@/lib/constants/contract_address";
 import FractionalizeIcon from "./icons/fractionalize-icon";
 import { Input } from "@/components/ui/input";
-import { Link } from "lucide-react";
+import Link from "next/link";
 
 const Fractionalize = () => {
   const [frequency, setFrequency] = useState("daily");
@@ -31,6 +31,8 @@ const Fractionalize = () => {
   const [ownerAddress, setOwnerAddress] = useState("");
   const { account } = useAccount();
   const [events, setEvents] = useState<any>();
+  const [loading, setLoading] = useState(false);
+  const [enableLinkProperty, setEnableLinkProperty] = useState(false);
 
   const handleFrequencyChange = (frequency: string) => setFrequency(frequency);
 
@@ -72,6 +74,8 @@ const Fractionalize = () => {
 
   const depositContract = async (propertyAddress: string) => {
     if (account) {
+      // state true
+      setLoading(true);
       const multiCall = await account.execute([
         // 1. transfer property
         {
@@ -99,7 +103,12 @@ const Fractionalize = () => {
           }),
         },
       ]);
-      await providerGoerli.waitForTransaction(multiCall.transaction_hash);
+      const receipt = await providerGoerli.waitForTransaction(
+        multiCall.transaction_hash
+      );
+      setLoading(false);
+      setEnableLinkProperty(true);
+      console.log(receipt);
     }
   };
 
@@ -147,97 +156,114 @@ const Fractionalize = () => {
   }, [abi]); // This effect runs when `abi` changes
 
   return (
-    <Card className="flex flex-col w-full md:w-[80%] sm:mx-auto text-white bg-[#111827A6]/65 py-5 px-5 sm:px-10 space-y-10 border-0">
-      <div className="w-full flex items-center justify-between">
-        <h1 className="text-2xl font-semibold">
-          Fractionalize your real estate
-        </h1>
-        <Button className="bg-[#16A24A] focus:bg-[#16A24A] hover:bg-[#16A24A]">
-          <CandleStickIcon />
-        </Button>
-      </div>
-      <CardHeader className="flex flex-col items-center justify-center space-y-5 border-2 border-slate-800 rounded-lg">
-        <h2 className="text-1xl font-semibold">
-          Enter your real estate address:{" "}
-        </h2>
-        <div className="flex flex-col items-center justify-center w-full space-y-8">
-          <div className="flex flex-col items-center justify-center space-y-1 w-[55%]">
-            <Input
-              type="text"
-              value={propertyAddress}
-              onChange={(event) =>
-                setPropertyAddress(event.target.value as string)
-              }
-              placeholder="Deposit Cairo Contract"
-              className="w-full bg-transparent px-5 py-7 border-slate-800 focus-visible:ring-offset-0 focus-visible:ring-0"
-            />
-          </div>
-        </div>
-        <p className="flex justify-center items-center w-full text-lg sm:text-xl text-white text-center">
-          Select counter access token frequency
-        </p>
-        <div className="flex space-x-2 justify-center sm:space-x-5">
-          <Button
-            onClick={() => handleFrequencyChange("daily")}
-            className={clsx(
-              "text-base text-white px-5 sm:px-10 border-2 border-[#16A24A]",
-              frequency === "daily" &&
-                "bg-[#16A24A] hover:bg-[#16A24A] focus:bg-[#16A24A]"
-            )}
-          >
-            Daily
-          </Button>
-          <Button
-            onClick={() => handleFrequencyChange("weekly")}
-            className={clsx(
-              "text-base text-white px-5 sm:px-10 border-2 border-[#16A24A]",
-              frequency === "weekly" &&
-                "bg-[#16A24A] hover:bg-[#16A24A] focus:bg-[#16A24A]"
-            )}
-          >
-            Weekly
-          </Button>
-          <Button
-            onClick={() => handleFrequencyChange("monthly")}
-            className={clsx(
-              "text-base text-white px-5 sm:px-10 border-2 border-[#16A24A]",
-              frequency === "monthly" &&
-                "bg-[#16A24A] hover:bg-[#16A24A] focus:bg-[#16A24A]"
-            )}
-          >
-            Monthly
+    <>
+      <Card className="flex flex-col w-full md:w-[80%] sm:mx-auto text-white bg-[#111827A6]/65 py-5 px-5 sm:px-10 space-y-10 border-0">
+        <div className="w-full flex items-center justify-between">
+          <h1 className="text-2xl font-semibold">
+            Fractionalize your real estate
+          </h1>
+          <Button className="bg-[#16A24A] focus:bg-[#16A24A] hover:bg-[#16A24A]">
+            <CandleStickIcon />
           </Button>
         </div>
-      </CardHeader>
-      <CardContent className="flex justify-center border-2 border-slate-800 rounded-lg pt-6 text-slate-300 text-lg sm:text-xl">
-        You are creating{" "}
-        <span className="font-bold ml-1 text-white">
-          {frequency === "daily" ? "7" : frequency === "weekly" ? "52" : "12"}{" "}
-          tokens
-        </span>
-      </CardContent>
-      <div className="flex">
-        <Button
-          className="bg-[#16A24A] hover:bg-[#16A24A] w-2/3 focus:bg-[#16A24A] flex justify-center items-center h-14 w-full"
-          onClick={() => depositContract(propertyAddress)}
-        >
-          <div className="flex flex-row justify-center items-center space-x-0.5">
-            <FractionalizeIcon />
-            <p className="mt-0.5 text-lg sm:text-xl font-normal">Deposit</p>
+        <CardHeader className="flex flex-col items-center justify-center space-y-5 border-2 border-slate-800 rounded-lg">
+          <h2 className="text-1xl font-semibold">
+            Enter your real estate address:{" "}
+          </h2>
+          <div className="flex flex-col items-center justify-center w-full space-y-8">
+            <div className="flex flex-col items-center justify-center space-y-1 w-[55%]">
+              <Input
+                type="text"
+                value={propertyAddress}
+                onChange={(event) =>
+                  setPropertyAddress(event.target.value as string)
+                }
+                placeholder="Deposit Cairo Contract"
+                className="w-full bg-transparent px-5 py-7 border-slate-800 focus-visible:ring-offset-0 focus-visible:ring-0"
+              />
+            </div>
           </div>
-        </Button>
-        <div>
-          {/* <p>status: {isPending && <div>Submitting...</div>}</p>
+          <p className="flex justify-center items-center w-full text-lg sm:text-xl text-white text-center">
+            Select counter access token frequency
+          </p>
+          <div className="flex space-x-2 justify-center sm:space-x-5">
+            <Button
+              onClick={() => handleFrequencyChange("daily")}
+              className={clsx(
+                "text-base text-white px-5 sm:px-10 border-2 border-[#16A24A]",
+                frequency === "daily" &&
+                  "bg-[#16A24A] hover:bg-[#16A24A] focus:bg-[#16A24A]"
+              )}
+            >
+              Minutely
+            </Button>
+            <Button
+              onClick={() => handleFrequencyChange("weekly")}
+              className={clsx(
+                "text-base text-white px-5 sm:px-10 border-2 border-[#16A24A]",
+                frequency === "weekly" &&
+                  "bg-[#16A24A] hover:bg-[#16A24A] focus:bg-[#16A24A]"
+              )}
+            >
+              Weekly
+            </Button>
+            <Button
+              onClick={() => handleFrequencyChange("monthly")}
+              className={clsx(
+                "text-base text-white px-5 sm:px-10 border-2 border-[#16A24A]",
+                frequency === "monthly" &&
+                  "bg-[#16A24A] hover:bg-[#16A24A] focus:bg-[#16A24A]"
+              )}
+            >
+              Monthly
+            </Button>
+          </div>
+        </CardHeader>
+        <CardContent className="flex justify-center border-2 border-slate-800 rounded-lg pt-6 text-slate-300 text-lg sm:text-xl">
+          You are creating{" "}
+          <span className="font-bold ml-1 text-white">
+            {frequency === "daily"
+              ? "2 tokens"
+              : frequency === "weekly"
+              ? "7 tokens"
+              : "12 tokens"}
+          </span>
+          {frequency === "daily"
+            ? ", the first for first of the minute and the other one for the second half"
+            : frequency === "weekly"
+            ? ", one for each day of the week"
+            : ", one for each month"}
+        </CardContent>
+        <div className="flex">
+          <Button
+            className="bg-[#16A24A] hover:bg-[#16A24A] w-2/3 focus:bg-[#16A24A] flex justify-center items-center h-14 w-full"
+            onClick={() => depositContract(propertyAddress)}
+            disabled={loading}
+          >
+            <div className="flex flex-row justify-center items-center space-x-0.5">
+              <FractionalizeIcon />
+              <p className="mt-0.5 text-lg sm:text-xl font-normal">Deposit</p>
+            </div>
+          </Button>
+          <div>
+            {/* <p>status: {isPending && <div>Submitting...</div>}</p>
           <p>hash: {data?.transaction_hash}</p> */}
+          </div>
         </div>
-      </div>
-      <button className="btn btn-primary">
-        Go the fractionalized contract - disable when contract is not deployed
-        yet
-      </button>
-      -------------- For debugging --------
-      <p> VaultContractAddress: {shortAddress(VAULT_ADDRESS)}</p>
-    </Card>
+        {/* <button className="btn btn-primary">
+          Go the fractionalized contract - disable when contract is not deployed
+          yet
+        </button> */}
+
+        {enableLinkProperty && (
+          <Link href={`/counters/fractionalized?contract=${propertyAddress}`}>
+            <Button className="btn btn-primary rounded-lg px-6 text-white bg-green-600 hover:bg-green-600 focus:bg-green-600">
+              Go to property
+            </Button>
+          </Link>
+        )}
+      </Card>
+    </>
   );
 };
 
