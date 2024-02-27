9d92df5c6a9391c746ca2488c83d2185c9184aed
= <zach@web3technologies.io>
2024-02-26 20:44:48 -0700

    add spinner component

--- src/components/flats-overview.tsx
+++ src/components/flats-overview.tsx
@@ -1,85 +1,35 @@
 "use client";
 
-import { useAccount } from "@starknet-react/core";
 import React, { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import CandleStickIcon from "./icons/candle-stick-icon";
 import Link from "next/link";
 import {
-  Account,
   RpcProvider,
   Contract,
-  CallData,
   num,
   hash,
-  provider,
 } from "starknet";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import clsx from "clsx";
+import { Card, } from "@/components/ui/card";
 import { VAULT_ADDRESS } from "@/lib/constants/contract_address";
 import Image from "next/image";
-import { PushSpinner, SwishSpinner } from "react-spinners-kit";
+import { SwishSpinner } from "react-spinners-kit";
 
-
-
-const shortAddress = (address: string) => {
-  let displayAddress = address?.slice(0, 5) + "..." + address?.slice(-4);
-  return displayAddress;
-};
+import { shortAddress } from "@/lib/utils";
 
 
 
 export const DisplayFlats = () => {
-  const [frequency, setFrequency] = useState("daily");
+
   const [abi, setAbi] = useState<any>();
   const [contract, setContract] = useState<any>();
-  const [propertyAddress, setPropertyAddress] = useState("");
-  const [ownerAddress, setOwnerAddress] = useState("");
-  const { account } = useAccount();
   const [events, setEvents] = useState<any>({events: []});
 
-  const handleFrequencyChange = (frequency: string) => setFrequency(frequency);
 
   const providerGoerli = new RpcProvider({
     nodeUrl: "https://starknet-testnet.public.blastapi.io/rpc/v0_6",
   });
 
-  const getAbi = async (provider: any, contractAddress: string) => {
-    const { abi: contractAbi } = await provider.getClassAt(contractAddress);
-    setAbi(contractAbi);
-    console.log("ABI", contractAbi);
-  };
-
-  const getOwner = async (propertyContract: any) => {
-    const owner = await contract.owner();
-    setOwnerAddress(`0x${owner.toString(16)}`);
-  };
-
-  const getContract = async (
-    abi: any,
-    contractAddress: string,
-    provider: any
-  ) => {
-    const contract = new Contract(abi, contractAddress, provider);
-    console.log(contract);
-    setContract(contract);
-  };
-
-  const callFunction = async () => {
-    const myCall = contract.populate("call_function", [
-      "0x04db71d3e903c931c9a5f7dfe10f7340277dfde6ff2a21995186a7f1207edc5b",
-      "toogle_door",
-      "",
-    ]);
-    contract.connect(account);
-    const res = await contract.call_function(myCall.calldata);
-    await providerGoerli.waitForTransaction(res.transaction_hash);
-  };
-
-  const shortAddress = (address: string) => {
-    let displayAddress = address?.slice(0, 5) + "..." + address?.slice(-4);
-    return displayAddress;
-  };
 
   const getEvents = async () => {
     const lastBlock = await providerGoerli.getBlockLatestAccepted();
@@ -123,7 +73,6 @@ export const DisplayFlats = () => {
     }
   }, [contract]);
 
-    console.log(events)
 
     return (
       <Card className="flex flex-col w-full mx-auto text-white bg-[#111827A6]/65 p-5 space-y-6 border-0">
